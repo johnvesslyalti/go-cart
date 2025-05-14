51bc146cab90432292473e9aec719a8c8ca25014
johnvesslyalti <altijohnvessly@gmail.com>
2025-05-14 08:46:26 +0530

    Update allowed origins for CORS to use the correct production URL

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,7 @@ connectDB()
 const app = express();
 
 const allowedOrigins = [
-    'https://go-cart-theta.vercel.app',
-    'https://go-cart-r2n80zspk-johnvessly-altis-projects.vercel.app/'  // Ensure no trailing slash
+    'https://go-cart-phi.vercel.app/'  // Ensure no trailing slash
 ];
 
 app.use(cors({
